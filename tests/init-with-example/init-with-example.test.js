4ac53362593f864399b7b0cba3669c8474a93d1c
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-02-24 11:45:45 -0500

    start testing with mocks, using jest@24

---
+++ tests/init-with-example/init-with-example.test.js
@@ -0,0 +1,64 @@
+const mockCallSnapshot = []
+
+mockPromptResponses = {
+  nativeModuleNameInput: { nativeModuleNameInput: 'test Module' },
+  isView: { isView: false },
+  confirmation: { confirmation: true },
+  modulePackageName: { modulePackageName: 'react-native-test-module' },
+  nativeObjectClassNamePrefixInput: {
+    nativeObjectClassNamePrefixInput: 'native'
+  },
+  platforms: { platforms: ['android', 'ios'] },
+  androidPackageId: { androidPackageId: 'com.test' },
+  authorName: { authorName: 'Ada' },
+  authorEmail: { authorEmail: 'ada@lovelace.name' },
+  license: { license: 'BSD-4-CLAUSE' },
+  generateExampleApp: { generateExampleApp: true },
+  showReactNativeOutput: { showReactNativeOutput: true }
+}
+
+jest.mock('prompts', () => args => {
+  mockCallSnapshot.push({ prompts: { args } })
+  const optionsArray = [].concat(args)
+  expect(optionsArray.length).toBe(1)
+  return Promise.resolve(mockPromptResponses[optionsArray[0].name])
+})
+
+jest.mock('execa', () => (cmd, args) => {
+  mockCallSnapshot.push({ execa: [cmd, args] })
+  if (cmd === 'git') {
+    if (args[1] == 'user.email')
+      return Promise.resolve({ stdout: 'alice@example.com' })
+    else
+      return Promise.resolve({
+        stdout: 'Alice'
+      })
+  } else {
+    return Promise.resolve()
+  }
+})
+
+jest.mock('create-react-native-module', () => o => {
+  mockCallSnapshot.push({ create: o })
+})
+
+jest.mock('fs-extra', () => ({
+  outputFile: (filePath, outputContents) => {
+    mockCallSnapshot.push({ outputFile: { filePath, outputContents } })
+  }
+}))
+
+jest.mock('path', () => ({
+  // quick solution to ignore first argument which is host-dependent
+  // value of process.cwd()
+  // better solution would be to use path.resolve() instead
+  join: (...parts) => ['$CWD'].concat(parts.slice(1)).join('/')
+}))
+
+it('generate native React Native module with example', async () => {
+  require('../../main')
+
+  await new Promise(resolve => setTimeout(resolve, 0.001))
+
+  expect(mockCallSnapshot).toMatchSnapshot()
+})
