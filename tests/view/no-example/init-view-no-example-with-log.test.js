284771e24d07365ee90ef2f085f0e501298944ea
Chris Brody <chris.brody+brodybits@gmail.com>
2020-02-27 14:33:44 -0500

    testing: add view tests with log (#16)
    * add view test with example, with log
    
    * add test of view with no example, with log
    
    * cleanup: completely remove extra fs-extra mock
    
    from init-view-no-example-with-log.test.js
    
    (not needed)
    
    * cleanup: fixed lint issue in view test using `yarn lint-fix`

---
+++ tests/view/no-example/init-view-no-example-with-log.test.js
@@ -0,0 +1,63 @@
+const mockCallSnapshot = []
+
+mockPromptResponses = {
+  nativeModuleNameInput: { nativeModuleNameInput: 'test View' },
+  isView: { isView: true },
+  confirmation: { confirmation: true },
+  modulePackageName: { modulePackageName: 'react-native-test-view' },
+  nativeObjectClassNamePrefixInput: {
+    nativeObjectClassNamePrefixInput: 'native'
+  },
+  platforms: { platforms: ['android', 'ios'] },
+  androidPackageId: { androidPackageId: 'com.test' },
+  authorName: { authorName: 'Ada' },
+  authorEmail: { authorEmail: 'ada@lovelace.name' },
+  license: { license: 'BSD-4-CLAUSE' },
+  generateExampleApp: { generateExampleApp: false }
+}
+
+jest.mock('console', () => ({
+  log: (...args) => {
+    mockCallSnapshot.push({ log: args })
+  }
+}))
+
+jest.mock('prompts', () => args => {
+  mockCallSnapshot.push({ prompts: { args } })
+  const optionsArray = [].concat(args)
+  expect(optionsArray.length).toBe(1)
+  return Promise.resolve(mockPromptResponses[optionsArray[0].name])
+})
+
+jest.mock('execa', () => (cmd, args) => {
+  mockCallSnapshot.push({ execa: [cmd, args] })
+  if (cmd === 'git') {
+    if (args[1] == 'user.email')
+      return Promise.resolve({ stdout: 'alice@example.com' })
+    else
+      return Promise.resolve({
+        stdout: 'Alice'
+      })
+  } else {
+    return Promise.resolve()
+  }
+})
+
+jest.mock('create-react-native-module', () => o => {
+  mockCallSnapshot.push({ create: o })
+})
+
+jest.mock('path', () => ({
+  // quick solution to ignore first argument which is host-dependent
+  // value of process.cwd()
+  // better solution would be to use path.resolve() instead
+  join: (...parts) => ['$CWD'].concat(parts.slice(1)).join('/')
+}))
+
+it('generate native React Native view with no example, with log', async () => {
+  require('../../../main')
+
+  await new Promise(resolve => setTimeout(resolve, 0.001))
+
+  expect(mockCallSnapshot).toMatchSnapshot()
+})
