2301fb398418cc57bbc62d48c22959e92e466aa1
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-03-01 14:21:24 -0500

    [TBD ...] test cleanup: simpler mock of prompts [...]
    with TBD comments to simplify snapshots

--- tests/tvos/with-example/tvos-with-example.test.js
+++ tests/tvos/with-example/tvos-with-example.test.js
@@ -27,12 +27,14 @@ jest.mock('console', () => ({
   }
 }))
 
-jest.mock('prompts', () => args => {
-  expect(Array.isArray(args)).toBe(true)
-  mockCallSnapshot.push({ prompts: { args } })
-  const optionsArray = [].concat(args)
-  expect(optionsArray.length).toBe(1)
-  return Promise.resolve(mockPromptResponses[optionsArray[0].name])
+jest.mock('prompts', () => prompts => {
+  expect(Array.isArray(prompts)).toBe(true)
+  expect(prompts.length).toBe(1)
+  // TBD SIMPLIFY snapshots
+  mockCallSnapshot.push({ prompts: { args: prompts } })
+  // like this:
+  // mockCallSnapshot.push({ prompts })
+  return Promise.resolve(mockPromptResponses[prompts[0].name])
 })
 
 jest.mock('execa', () => (cmd, args, opts) => {
