3d72e4523e1c408e4e691bf423319d9a6e6dabba
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-03-01 16:16:11 -0500

    add test with option to generate with sample use of Apple Networking

---
+++ tests/with-apple-networking/with-apple-networking.test.js
@@ -0,0 +1,60 @@
+const mockCallSnapshot = []
+
+mockPromptResponses = {
+  nativeModuleNameInput: { nativeModuleNameInput: 'test Module' },
+  isView: { isView: false },
+  confirmation: { confirmation: true },
+  modulePackageName: { modulePackageName: 'react-native-test-module' },
+  nativeObjectClassNamePrefixInput: {
+    nativeObjectClassNamePrefixInput: 'native'
+  },
+  platforms: { platforms: ['android', 'ios'] },
+  androidPackageId: { androidPackageId: 'com.test' },
+  tvosEnabled: { tvosEnabled: false },
+  authorName: { authorName: 'Ada' },
+  authorEmail: { authorEmail: 'ada@lovelace.name' },
+  useAppleNetworking: { useAppleNetworking: true },
+  githubUserAccountName: { githubUserAccountName: 'ada-lovelace' },
+  license: { license: 'BSD-4-CLAUSE' },
+  generateExampleApp: { generateExampleApp: false }
+}
+
+jest.mock('console', () => ({
+  log: (...args) => {
+    /* do nothing */
+  }
+}))
+
+jest.mock('prompts', () => args => {
+  expect(Array.isArray(args)).toBe(true)
+  mockCallSnapshot.push({ prompts: { args } })
+  const optionsArray = [].concat(args)
+  expect(optionsArray.length).toBe(1)
+  return Promise.resolve(mockPromptResponses[optionsArray[0].name])
+})
+
+jest.mock('execa', () => (cmd, args, opts) => {
+  mockCallSnapshot.push({ execa: [cmd, args, opts] })
+  if (cmd === 'git') {
+    if (args[1] == 'user.email')
+      return Promise.resolve({ stdout: 'alice@example.com' })
+    else
+      return Promise.resolve({
+        stdout: 'Alice'
+      })
+  } else {
+    return Promise.resolve()
+  }
+})
+
+jest.mock('create-react-native-module', () => o => {
+  mockCallSnapshot.push({ create: o })
+})
+
+it('generate native React Native module with no example, with log', async () => {
+  require('../../main')
+
+  await new Promise(resolve => setTimeout(resolve, 0.001))
+
+  expect(mockCallSnapshot).toMatchSnapshot()
+})
