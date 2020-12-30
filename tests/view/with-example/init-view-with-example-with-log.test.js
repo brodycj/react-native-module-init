54ca5433e84b405f6e8bfe3dd45d3f54133866ba
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-12-30 00:20:32 -0500

    use Prettier to fix tests

--- tests/view/with-example/init-view-with-example-with-log.test.js
+++ tests/view/with-example/init-view-with-example-with-log.test.js
@@ -1,83 +1,83 @@
-const mockCallSnapshot = [];
+const mockCallSnapshot = []
 
 const mockPromptResponses = {
-  nativeModuleName: { nativeModuleName: "test View" },
+  nativeModuleName: { nativeModuleName: 'test View' },
   isView: { isView: true },
   confirmation: { confirmation: true },
-  modulePackageName: { modulePackageName: "react-native-test-view" },
+  modulePackageName: { modulePackageName: 'react-native-test-view' },
   nativeObjectClassNamePrefixInput: {
-    nativeObjectClassNamePrefixInput: "native",
+    nativeObjectClassNamePrefixInput: 'native'
   },
-  nativeObjectClassName: { nativeObjectClassName: "SUPERTestView" },
-  platforms: { platforms: ["android", "ios"] },
-  androidPackageId: { androidPackageId: "com.test" },
+  nativeObjectClassName: { nativeObjectClassName: 'SUPERTestView' },
+  platforms: { platforms: ['android', 'ios'] },
+  androidPackageId: { androidPackageId: 'com.test' },
   tvosEnabled: { tvosEnabled: false },
-  authorName: { authorName: "Ada" },
-  authorEmail: { authorEmail: "ada@lovelace.name" },
-  githubUserAccountName: { githubUserAccountName: "ada-lovelace" },
-  license: { license: "BSD-4-CLAUSE" },
+  authorName: { authorName: 'Ada' },
+  authorEmail: { authorEmail: 'ada@lovelace.name' },
+  githubUserAccountName: { githubUserAccountName: 'ada-lovelace' },
+  license: { license: 'BSD-4-CLAUSE' },
   generateExampleApp: { generateExampleApp: true },
   useAppleNetworking: { useAppleNetworking: false },
-  exampleTemplate: { exampleTemplate: "react-native@latest" },
-  exampleAppName: { exampleAppName: "example" },
-};
+  exampleTemplate: { exampleTemplate: 'react-native@latest' },
+  exampleAppName: { exampleAppName: 'example' }
+}
 
-jest.mock("console", () => ({
+jest.mock('console', () => ({
   log: (...args) => {
-    mockCallSnapshot.push({ log: args });
-  },
-}));
+    mockCallSnapshot.push({ log: args })
+  }
+}))
 
-jest.mock("prompts", () => (args) => {
-  expect(Array.isArray(args)).toBe(true);
-  mockCallSnapshot.push({ prompts: { args } });
-  const optionsArray = [].concat(args);
-  expect(optionsArray.length).toBe(1);
-  return Promise.resolve(mockPromptResponses[optionsArray[0].name]);
-});
+jest.mock('prompts', () => args => {
+  expect(Array.isArray(args)).toBe(true)
+  mockCallSnapshot.push({ prompts: { args } })
+  const optionsArray = [].concat(args)
+  expect(optionsArray.length).toBe(1)
+  return Promise.resolve(mockPromptResponses[optionsArray[0].name])
+})
 
-jest.mock("execa", () => (cmd, args, opts) => {
-  mockCallSnapshot.push({ execa: [cmd, args, opts] });
-  if (cmd === "git") {
-    if (args[1] === "user.email") {
-      return Promise.resolve({ stdout: "alice@example.com" });
+jest.mock('execa', () => (cmd, args, opts) => {
+  mockCallSnapshot.push({ execa: [cmd, args, opts] })
+  if (cmd === 'git') {
+    if (args[1] === 'user.email') {
+      return Promise.resolve({ stdout: 'alice@example.com' })
     } else {
       return Promise.resolve({
-        stdout: "Alice",
-      });
+        stdout: 'Alice'
+      })
     }
   } else {
-    return Promise.resolve();
+    return Promise.resolve()
   }
-});
+})
 
-jest.mock("create-react-native-module", () => (o) => {
-  mockCallSnapshot.push({ create: o });
-});
+jest.mock('create-react-native-module', () => o => {
+  mockCallSnapshot.push({ create: o })
+})
 
-jest.mock("fs-extra", () => ({
+jest.mock('fs-extra', () => ({
   outputFile: (filePath, outputContents) => {
-    mockCallSnapshot.push({ outputFile: { filePath, outputContents } });
-  },
-}));
+    mockCallSnapshot.push({ outputFile: { filePath, outputContents } })
+  }
+}))
 
-jest.mock("path", () => ({
+jest.mock('path', () => ({
   // quick solution to use & log system-independent paths in the snapshots,
   // with none of the arguments ignored
   resolve: (...paths) =>
-    `/home/ada.lovelace/path_resolved_from_${paths.join("/")}`,
+    `/home/ada.lovelace/path_resolved_from_${paths.join('/')}`,
   // support functionality of *real* path join operation
-  join: (...paths) => [].concat(paths).join("/"),
-}));
+  join: (...paths) => [].concat(paths).join('/')
+}))
 
-jest.mock("react-native-init-func", () => (...args) => {
-  mockCallSnapshot.push({ reactNativeInit: args });
-});
+jest.mock('react-native-init-func', () => (...args) => {
+  mockCallSnapshot.push({ reactNativeInit: args })
+})
 
-it("generate native React Native view with example, with log", async () => {
-  require("../../../main");
+it('generate native React Native view with example, with log', async () => {
+  require('../../../main')
 
-  await new Promise((resolve) => setTimeout(resolve, 0.001));
+  await new Promise(resolve => setTimeout(resolve, 0.001))
 
-  expect(mockCallSnapshot).toMatchSnapshot();
-});
+  expect(mockCallSnapshot).toMatchSnapshot()
+})
