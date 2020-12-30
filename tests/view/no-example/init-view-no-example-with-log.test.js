2b2efa8d994b55794fc9719b100c9eda41d1ec51
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-12-30 12:41:14 -0500

    Revert "do `npx prettier --write tests` to resolve the affected if statements"
    This reverts commit de936dca68be9843f75e4fcea638512341e4b5da.

--- tests/view/no-example/init-view-no-example-with-log.test.js
+++ tests/view/no-example/init-view-no-example-with-log.test.js
@@ -1,62 +1,60 @@
-const mockCallSnapshot = [];
+const mockCallSnapshot = []
 
 const mockPromptResponses = {
-  nativeModuleName: { nativeModuleName: "test View" },
+  nativeModuleName: { nativeModuleName: 'test View' },
   isView: { isView: true },
   confirmation: { confirmation: true },
-  modulePackageName: { modulePackageName: "react-native-test-view" },
+  modulePackageName: { modulePackageName: 'react-native-test-view' },
   nativeObjectClassNamePrefixInput: {
-    nativeObjectClassNamePrefixInput: "native",
+    nativeObjectClassNamePrefixInput: 'native'
   },
-  nativeObjectClassName: { nativeObjectClassName: "SUPERTestView" },
-  platforms: { platforms: ["android", "ios"] },
-  androidPackageId: { androidPackageId: "com.test" },
+  nativeObjectClassName: { nativeObjectClassName: 'SUPERTestView' },
+  platforms: { platforms: ['android', 'ios'] },
+  androidPackageId: { androidPackageId: 'com.test' },
   tvosEnabled: { tvosEnabled: false },
-  authorName: { authorName: "Ada" },
-  authorEmail: { authorEmail: "ada@lovelace.name" },
-  githubUserAccountName: { githubUserAccountName: "ada-lovelace" },
+  authorName: { authorName: 'Ada' },
+  authorEmail: { authorEmail: 'ada@lovelace.name' },
+  githubUserAccountName: { githubUserAccountName: 'ada-lovelace' },
   useAppleNetworking: { useAppleNetworking: false },
-  license: { license: "BSD-4-CLAUSE" },
-  generateExampleApp: { generateExampleApp: false },
-};
+  license: { license: 'BSD-4-CLAUSE' },
+  generateExampleApp: { generateExampleApp: false }
+}
 
-jest.mock("console", () => ({
+jest.mock('console', () => ({
   log: (...args) => {
-    mockCallSnapshot.push({ log: args });
-  },
-}));
+    mockCallSnapshot.push({ log: args })
+  }
+}))
 
-jest.mock("prompts", () => (args) => {
-  expect(Array.isArray(args)).toBe(true);
-  mockCallSnapshot.push({ prompts: { args } });
-  const optionsArray = [].concat(args);
-  expect(optionsArray.length).toBe(1);
-  return Promise.resolve(mockPromptResponses[optionsArray[0].name]);
-});
+jest.mock('prompts', () => args => {
+  expect(Array.isArray(args)).toBe(true)
+  mockCallSnapshot.push({ prompts: { args } })
+  const optionsArray = [].concat(args)
+  expect(optionsArray.length).toBe(1)
+  return Promise.resolve(mockPromptResponses[optionsArray[0].name])
+})
 
-jest.mock("execa", () => (cmd, args, opts) => {
-  mockCallSnapshot.push({ execa: [cmd, args, opts] });
-  if (cmd === "git") {
-    if (args[1] === "user.email") {
-      return Promise.resolve({ stdout: "alice@example.com" });
-    } else {
+jest.mock('execa', () => (cmd, args, opts) => {
+  mockCallSnapshot.push({ execa: [cmd, args, opts] })
+  if (cmd === 'git') {
+    if (args[1] === 'user.email') { return Promise.resolve({ stdout: 'alice@example.com' }) } else {
       return Promise.resolve({
-        stdout: "Alice",
-      });
+        stdout: 'Alice'
+      })
     }
   } else {
-    return Promise.resolve();
+    return Promise.resolve()
   }
-});
+})
 
-jest.mock("create-react-native-module", () => (o) => {
-  mockCallSnapshot.push({ create: o });
-});
+jest.mock('create-react-native-module', () => o => {
+  mockCallSnapshot.push({ create: o })
+})
 
-it("generate native React Native view with no example, with log", async () => {
-  require("../../../main");
+it('generate native React Native view with no example, with log', async () => {
+  require('../../../main')
 
-  await new Promise((resolve) => setTimeout(resolve, 0.001));
+  await new Promise(resolve => setTimeout(resolve, 0.001))
 
-  expect(mockCallSnapshot).toMatchSnapshot();
-});
+  expect(mockCallSnapshot).toMatchSnapshot()
+})
