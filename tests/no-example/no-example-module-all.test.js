de936dca68be9843f75e4fcea638512341e4b5da
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-12-29 23:06:20 -0500

    do `npx prettier --write tests` to resolve the affected if statements

--- tests/no-example/no-example-module-all.test.js
+++ tests/no-example/no-example-module-all.test.js
@@ -1,66 +1,68 @@
-const mockCallSnapshot = []
+const mockCallSnapshot = [];
 
 const mockPromptResponses = {
-  nativeModuleName: { nativeModuleName: 'test Module' },
+  nativeModuleName: { nativeModuleName: "test Module" },
   isView: { isView: false },
   confirmation: { confirmation: true },
-  modulePackageName: { modulePackageName: 'react-native-test-module' },
+  modulePackageName: { modulePackageName: "react-native-test-module" },
   nativeObjectClassNamePrefixInput: {
-    nativeObjectClassNamePrefixInput: 'native'
+    nativeObjectClassNamePrefixInput: "native",
   },
-  nativeObjectClassName: { nativeObjectClassName: 'SUPERTestModule' },
-  platforms: { platforms: ['android', 'ios'] },
-  androidPackageId: { androidPackageId: 'com.test' },
+  nativeObjectClassName: { nativeObjectClassName: "SUPERTestModule" },
+  platforms: { platforms: ["android", "ios"] },
+  androidPackageId: { androidPackageId: "com.test" },
   tvosEnabled: { tvosEnabled: false },
-  authorName: { authorName: 'Ada' },
-  authorEmail: { authorEmail: 'ada@lovelace.name' },
+  authorName: { authorName: "Ada" },
+  authorEmail: { authorEmail: "ada@lovelace.name" },
   useAppleNetworking: { useAppleNetworking: false },
-  githubUserAccountName: { githubUserAccountName: 'ada-lovelace' },
-  license: { license: 'BSD-4-CLAUSE' },
-  generateExampleApp: { generateExampleApp: false }
-}
+  githubUserAccountName: { githubUserAccountName: "ada-lovelace" },
+  license: { license: "BSD-4-CLAUSE" },
+  generateExampleApp: { generateExampleApp: false },
+};
 
-jest.mock('please-upgrade-node', () => ({ name, engines }) => {
+jest.mock("please-upgrade-node", () => ({ name, engines }) => {
   // capture & check limited info as retrieved from package.json,
   // no need to check other items such as version or dependencies
-  mockCallSnapshot.push({ 'please-upgrade-node': { name, engines } })
-})
+  mockCallSnapshot.push({ "please-upgrade-node": { name, engines } });
+});
 
-jest.mock('console', () => ({
+jest.mock("console", () => ({
   log: (...args) => {
-    mockCallSnapshot.push({ log: args })
-  }
-}))
+    mockCallSnapshot.push({ log: args });
+  },
+}));
 
-jest.mock('prompts', () => args => {
-  expect(Array.isArray(args)).toBe(true)
-  mockCallSnapshot.push({ prompts: { args } })
-  const optionsArray = [].concat(args)
-  expect(optionsArray.length).toBe(1)
-  return Promise.resolve(mockPromptResponses[optionsArray[0].name])
-})
+jest.mock("prompts", () => (args) => {
+  expect(Array.isArray(args)).toBe(true);
+  mockCallSnapshot.push({ prompts: { args } });
+  const optionsArray = [].concat(args);
+  expect(optionsArray.length).toBe(1);
+  return Promise.resolve(mockPromptResponses[optionsArray[0].name]);
+});
 
-jest.mock('execa', () => (cmd, args, opts) => {
-  mockCallSnapshot.push({ execa: [cmd, args, opts] })
-  if (cmd === 'git') {
-    if (args[1] === 'user.email') { return Promise.resolve({ stdout: 'alice@example.com' }) } else {
+jest.mock("execa", () => (cmd, args, opts) => {
+  mockCallSnapshot.push({ execa: [cmd, args, opts] });
+  if (cmd === "git") {
+    if (args[1] === "user.email") {
+      return Promise.resolve({ stdout: "alice@example.com" });
+    } else {
       return Promise.resolve({
-        stdout: 'Alice'
-      })
+        stdout: "Alice",
+      });
     }
   } else {
-    return Promise.resolve()
+    return Promise.resolve();
   }
-})
+});
 
-jest.mock('create-react-native-module', () => o => {
-  mockCallSnapshot.push({ create: o })
-})
+jest.mock("create-react-native-module", () => (o) => {
+  mockCallSnapshot.push({ create: o });
+});
 
-it('require `please-upgrade-node`, generate module with no example, with correct logging', async () => {
-  require('../../main')
+it("require `please-upgrade-node`, generate module with no example, with correct logging", async () => {
+  require("../../main");
 
-  await new Promise(resolve => setTimeout(resolve, 0.001))
+  await new Promise((resolve) => setTimeout(resolve, 0.001));
 
-  expect(mockCallSnapshot).toMatchSnapshot()
-})
+  expect(mockCallSnapshot).toMatchSnapshot();
+});
