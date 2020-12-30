e058c1884b4da681b58ac820d111a68f17962b0e
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-12-30 12:40:51 -0500

    Revert "use `lint:fix` to fix formatting of tests"
    This reverts commit e9c2c8146de4abaabd35e551bcc27261beee1bc7.

--- tests/init-with-example/init-with-example-with-log.test.js
+++ tests/init-with-example/init-with-example-with-log.test.js
@@ -1,85 +1,85 @@
-const mockCallSnapshot = []
+const mockCallSnapshot = [];
 
 const mockPromptResponses = {
-  nativeModuleName: { nativeModuleName: 'test Module' },
+  nativeModuleName: { nativeModuleName: "test Module" },
   isView: { isView: false },
   confirmation: { confirmation: true },
-  modulePackageName: { modulePackageName: 'react-native-test-module' },
+  modulePackageName: { modulePackageName: "react-native-test-module" },
   nativeObjectClassNamePrefixInput: {
-    nativeObjectClassNamePrefixInput: 'native'
+    nativeObjectClassNamePrefixInput: "native",
   },
-  nativeObjectClassName: { nativeObjectClassName: 'SUPERTestModule' },
-  platforms: { platforms: ['android', 'ios'] },
-  androidPackageId: { androidPackageId: 'com.test' },
+  nativeObjectClassName: { nativeObjectClassName: "SUPERTestModule" },
+  platforms: { platforms: ["android", "ios"] },
+  androidPackageId: { androidPackageId: "com.test" },
   tvosEnabled: { tvosEnabled: false },
-  authorName: { authorName: 'Ada' },
-  authorEmail: { authorEmail: 'ada@lovelace.name' },
-  githubUserAccountName: { githubUserAccountName: 'ada-lovelace' },
-  license: { license: 'BSD-4-CLAUSE' },
+  authorName: { authorName: "Ada" },
+  authorEmail: { authorEmail: "ada@lovelace.name" },
+  githubUserAccountName: { githubUserAccountName: "ada-lovelace" },
+  license: { license: "BSD-4-CLAUSE" },
   useAppleNetworking: { useAppleNetworking: false },
   generateExampleApp: { generateExampleApp: true },
-  exampleTemplate: { exampleTemplate: 'react-native@latest' },
-  exampleAppName: { exampleAppName: 'example' }
-}
+  exampleTemplate: { exampleTemplate: "react-native@latest" },
+  exampleAppName: { exampleAppName: "example" },
+};
 
-jest.mock('console', () => ({
+jest.mock("console", () => ({
   log: (...args) => {
-    mockCallSnapshot.push({ log: args })
-  }
-}))
+    mockCallSnapshot.push({ log: args });
+  },
+}));
 
-jest.mock('prompts', () => args => {
-  expect(Array.isArray(args)).toBe(true)
-  mockCallSnapshot.push({ prompts: { args } })
-  const optionsArray = [].concat(args)
-  expect(optionsArray.length).toBe(1)
-  return Promise.resolve(mockPromptResponses[optionsArray[0].name])
-})
+jest.mock("prompts", () => (args) => {
+  expect(Array.isArray(args)).toBe(true);
+  mockCallSnapshot.push({ prompts: { args } });
+  const optionsArray = [].concat(args);
+  expect(optionsArray.length).toBe(1);
+  return Promise.resolve(mockPromptResponses[optionsArray[0].name]);
+});
 
-jest.mock('execa', () => (cmd, args, opts) => {
-  mockCallSnapshot.push({ execa: [cmd, args, opts] })
-  if (cmd === 'git') {
-    if (args[1] === 'user.email') {
-      return Promise.resolve({ stdout: 'alice@example.com' })
+jest.mock("execa", () => (cmd, args, opts) => {
+  mockCallSnapshot.push({ execa: [cmd, args, opts] });
+  if (cmd === "git") {
+    if (args[1] === "user.email") {
+      return Promise.resolve({ stdout: "alice@example.com" });
     } else {
       return Promise.resolve({
-        stdout: 'Alice'
-      })
+        stdout: "Alice",
+      });
     }
   } else {
-    return Promise.resolve()
+    return Promise.resolve();
   }
-})
+});
 
-jest.mock('create-react-native-module', () => o => {
-  mockCallSnapshot.push({ create: o })
-})
+jest.mock("create-react-native-module", () => (o) => {
+  mockCallSnapshot.push({ create: o });
+});
 
-jest.mock('fs-extra', () => ({
+jest.mock("fs-extra", () => ({
   outputFile: (filePath, outputContents) => {
-    mockCallSnapshot.push({ outputFile: { filePath, outputContents } })
-  }
-}))
+    mockCallSnapshot.push({ outputFile: { filePath, outputContents } });
+  },
+}));
 
-jest.mock('path', () => ({
+jest.mock("path", () => ({
   // quick solution to use & log system-independent paths in the snapshots,
   // with none of the arguments ignored
   resolve: (...paths) =>
-    paths.length === 1 && paths[0] === '.'
-      ? '/home/ada.lovelace/path_resolved_from'
-      : paths.join('/'),
+    paths.length === 1 && paths[0] === "."
+      ? "/home/ada.lovelace/path_resolved_from"
+      : paths.join("/"),
   // support functionality of *real* path join operation
-  join: (...paths) => [].concat(paths).join('/')
-}))
+  join: (...paths) => [].concat(paths).join("/"),
+}));
 
-jest.mock('react-native-init-func', () => (...args) => {
-  mockCallSnapshot.push({ reactNativeInit: args })
-})
+jest.mock("react-native-init-func", () => (...args) => {
+  mockCallSnapshot.push({ reactNativeInit: args });
+});
 
-it('generate native React Native module with example, with log', async () => {
-  require('../../main')
+it("generate native React Native module with example, with log", async () => {
+  require("../../main");
 
-  await new Promise(resolve => setTimeout(resolve, 0.001))
+  await new Promise((resolve) => setTimeout(resolve, 0.001));
 
-  expect(mockCallSnapshot).toMatchSnapshot()
-})
+  expect(mockCallSnapshot).toMatchSnapshot();
+});
--- tests/with-apple-networking/with-apple-networking.test.js
+++ tests/with-apple-networking/with-apple-networking.test.js
@@ -1,62 +1,62 @@
-const mockCallSnapshot = []
+const mockCallSnapshot = [];
 
 const mockPromptResponses = {
-  nativeModuleName: { nativeModuleName: 'test Module' },
+  nativeModuleName: { nativeModuleName: "test Module" },
   isView: { isView: false },
   confirmation: { confirmation: true },
-  modulePackageName: { modulePackageName: 'react-native-test-module' },
+  modulePackageName: { modulePackageName: "react-native-test-module" },
   nativeObjectClassNamePrefixInput: {
-    nativeObjectClassNamePrefixInput: 'native'
+    nativeObjectClassNamePrefixInput: "native",
   },
-  nativeObjectClassName: { nativeObjectClassName: 'SUPERTestModule' },
-  platforms: { platforms: ['android', 'ios'] },
-  androidPackageId: { androidPackageId: 'com.test' },
+  nativeObjectClassName: { nativeObjectClassName: "SUPERTestModule" },
+  platforms: { platforms: ["android", "ios"] },
+  androidPackageId: { androidPackageId: "com.test" },
   tvosEnabled: { tvosEnabled: false },
-  authorName: { authorName: 'Ada' },
-  authorEmail: { authorEmail: 'ada@lovelace.name' },
+  authorName: { authorName: "Ada" },
+  authorEmail: { authorEmail: "ada@lovelace.name" },
   useAppleNetworking: { useAppleNetworking: true },
-  githubUserAccountName: { githubUserAccountName: 'ada-lovelace' },
-  license: { license: 'BSD-4-CLAUSE' },
-  generateExampleApp: { generateExampleApp: false }
-}
+  githubUserAccountName: { githubUserAccountName: "ada-lovelace" },
+  license: { license: "BSD-4-CLAUSE" },
+  generateExampleApp: { generateExampleApp: false },
+};
 
-jest.mock('console', () => ({
+jest.mock("console", () => ({
   log: (...args) => {
     /* do nothing */
-  }
-}))
+  },
+}));
 
-jest.mock('prompts', () => args => {
-  expect(Array.isArray(args)).toBe(true)
-  mockCallSnapshot.push({ prompts: { args } })
-  const optionsArray = [].concat(args)
-  expect(optionsArray.length).toBe(1)
-  return Promise.resolve(mockPromptResponses[optionsArray[0].name])
-})
+jest.mock("prompts", () => (args) => {
+  expect(Array.isArray(args)).toBe(true);
+  mockCallSnapshot.push({ prompts: { args } });
+  const optionsArray = [].concat(args);
+  expect(optionsArray.length).toBe(1);
+  return Promise.resolve(mockPromptResponses[optionsArray[0].name]);
+});
 
-jest.mock('execa', () => (cmd, args, opts) => {
-  mockCallSnapshot.push({ execa: [cmd, args, opts] })
-  if (cmd === 'git') {
-    if (args[1] === 'user.email') {
-      return Promise.resolve({ stdout: 'alice@example.com' })
+jest.mock("execa", () => (cmd, args, opts) => {
+  mockCallSnapshot.push({ execa: [cmd, args, opts] });
+  if (cmd === "git") {
+    if (args[1] === "user.email") {
+      return Promise.resolve({ stdout: "alice@example.com" });
     } else {
       return Promise.resolve({
-        stdout: 'Alice'
-      })
+        stdout: "Alice",
+      });
     }
   } else {
-    return Promise.resolve()
+    return Promise.resolve();
   }
-})
+});
 
-jest.mock('create-react-native-module', () => o => {
-  mockCallSnapshot.push({ create: o })
-})
+jest.mock("create-react-native-module", () => (o) => {
+  mockCallSnapshot.push({ create: o });
+});
 
-it('generate native React Native module with no example, with log', async () => {
-  require('../../main')
+it("generate native React Native module with no example, with log", async () => {
+  require("../../main");
 
-  await new Promise(resolve => setTimeout(resolve, 0.001))
+  await new Promise((resolve) => setTimeout(resolve, 0.001));
 
-  expect(mockCallSnapshot).toMatchSnapshot()
-})
+  expect(mockCallSnapshot).toMatchSnapshot();
+});
