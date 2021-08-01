b5167e87b2cedcf9aa9cc0f2e367683d433928a8
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-08-01 18:19:16 -0400

    lint (Prettier) fix

--- main.js
+++ main.js
@@ -152,7 +152,9 @@ Promise.resolve().then(async () => {
   const { modulePackageName } = await prompt({
     type: 'text',
     name: 'modulePackageName',
-    message: `What is the full native ${isView ? 'view' : 'module'} package name?`,
+    message: `What is the full native ${
+      isView ? 'view' : 'module'
+    } package name?`,
     initial: initialModulePackageName,
     validate: modulePackageName => modulePackageName.length > 0
   })
